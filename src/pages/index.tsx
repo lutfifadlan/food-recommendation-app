25bc69406bc294bc78bc4a212cf272e07a41c024
lutfifadlan <mochamadlutfifadlan@gmail.com>
2024-08-09 10:24:01 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -70,55 +70,48 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-light dark:bg-dark">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-light dark:bg-dark px-4">
       <Meteors number={50} />
-      <h1 className="text-3xl font-bold mb-2 transition-colors duration-300 flex items-center justify-center">
+      <h1 className="text-2xl md:text-3xl font-bold mb-2 transition-colors duration-300 flex items-center justify-center text-center">
         {t('title1')}
       </h1>
-      <h1 className="text-3xl font-bold mb-2 transition-colors duration-300 flex items-center justify-center">
+      <h1 className="text-2xl md:text-3xl font-bold mb-2 transition-colors duration-300 flex items-center justify-center text-center">
         {t('title2')}
       </h1>
       
-      <div className="w-3/4">
-        <div className="flex items-center justify-center">
+      <div className="w-full md:w-3/4">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-2">
           <ModeToggle />
-          &nbsp;
-          &nbsp;
           <LanguageSwitcher />
-          &nbsp;
-          &nbsp;
           <Input
             type="text"
             placeholder={t('inputPlaceholder')}
-            className="w-72 mt-4 mb-4"
+            className="w-full md:w-72 mt-4 mb-4"
             value={location}
             onChange={(e) => setLocation(e.target.value)}
           />
         </div>
       </div>
-      <Button onClick={handleGenerate} disabled={loading} className="flex flex-col items-center justify-center">
-            {loading ? t('generatingButton') : t('generateButton')}
-        </Button>
-      <div className="w-1/2 mt-4">
+      <Button onClick={handleGenerate} disabled={loading} className="flex items-center justify-center mt-4">
+        {loading ? t('generatingButton') : t('generateButton')}
+      </Button>
+      <div className="w-full md:w-1/2 mt-4 px-4 md:px-0">
         {recommendation.breakfast && (
-          <div>
+          <div className="mb-4">
             <strong>{t('breakfast')}</strong>
             <ReactMarkdown>{recommendation.breakfast}</ReactMarkdown>
-            <br />
           </div>
         )}
         {recommendation.lunch && (
-          <div>
+          <div className="mb-4">
             <strong>{t('lunch')}</strong>
             <ReactMarkdown>{recommendation.lunch}</ReactMarkdown>
-            <br />
           </div>
         )}
         {recommendation.dinner && (
-          <div>
+          <div className="mb-4">
             <strong>{t('dinner')}</strong>
             <ReactMarkdown>{recommendation.dinner}</ReactMarkdown>
-            <br />
           </div>
         )}
       </div>
